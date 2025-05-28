d1fb0956e87f3f7ab20e8301c5f2dcb57545b4ee
Stephen Zhou <zzstephen@gmail.com>
2025-05-28 11:34:30 -0400

    add data prep config

--- Data_prep/data_config.py
+++ Data_prep/data_config.py
@@ -16,15 +16,15 @@ segments = [0,1,2]
 
 dummy_grouping = {}
 
-dummy_grouping['segment0'] = 'dummy_grouping_segment0.yaml'
-dummy_grouping['segment1'] = 'dummy_grouping_segment1.yaml'
-dummy_grouping['segment2'] = 'dummy_grouping_segment2.yaml'
+dummy_grouping['segment0'] = 'dummy_grouping_segment0_draft.yaml'
+dummy_grouping['segment1'] = 'dummy_grouping_segment1_draft.yaml'
+dummy_grouping['segment2'] = 'dummy_grouping_segment2_draft.yaml'
 
 knots_1d = {}
 
-knots_1d['segment0']= '1d_knots_segment0.yaml'
-knots_1d['segment1']= '1d_knots_segment1.yaml'
-knots_1d['segment2']= '1d_knots_segment2.yaml'
+knots_1d['segment0']= '1d_knots_segment0_draft.yaml'
+knots_1d['segment1']= '1d_knots_segment1_draft.yaml'
+knots_1d['segment2']= '1d_knots_segment2_draft.yaml'
 
 
 
